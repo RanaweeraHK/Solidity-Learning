b3faa59606b31a85c64eceeed13387afd5220195
RanaweeraHK <ranaweerahk@gmail.com>
2025-07-17 11:44:10 +0530

    Add the Exercises and Solutions form NaziWeb ultimate solidity course

---
+++ contracts/4_hello.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.17;
+// which solidity compiler goes to compile my contract
+
+contract Hello {
+    string public greet = "Hello World";
+
+    uint256 public  waiting = 1 days;
+}
